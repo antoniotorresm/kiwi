c9e705adb5cdccd9ffea25760bf094d716d202bc
Antonio Torres Moríñigo <atorresm@protonmail.com>
2018-04-26 13:06:46 +0200

    Completed Google Calendar API interaction

--- src/main/java/aiss/model/resource/GoogleCalendarResource.java
+++ src/main/java/aiss/model/resource/GoogleCalendarResource.java
@@ -3,7 +3,10 @@ package aiss.model.resource;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,8 +14,12 @@ import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
 import com.google.api.services.calendar.Calendar;
 import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.EventAttendee;
+import com.google.api.services.calendar.model.EventDateTime;
 
 public class GoogleCalendarResource {
 	private static final Logger log = Logger.getLogger(GoogleCalendarResource.class.getName());
@@ -32,10 +39,8 @@ public class GoogleCalendarResource {
 			calendarAdmin = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential).setApplicationName("Kiwi")
 					.build();
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -43,6 +48,48 @@ public class GoogleCalendarResource {
 	public com.google.api.services.calendar.model.Calendar getPrimaryCalendar() throws IOException {
 		log.log(Level.FINE, "Primary calendar retrieved");
 		return calendarAdmin.calendars().get("primary").execute();
+	}
+
+	public void createEvent(String name, String description, String location, DateTime startDateTime,
+			DateTime endDateTime, String creatorEmail) throws IOException {
+		Event event = new Event().setSummary(name).setLocation(location).setDescription(description);
+		EventDateTime start = new EventDateTime().setDateTime(startDateTime).setTimeZone("Europe/Madrid");
+		event.setStart(start);
+		EventDateTime end = new EventDateTime().setDateTime(endDateTime).setTimeZone("Europe/Madrid");
+		event.setEnd(end);
+		event.setAnyoneCanAddSelf(true);
+		event.setVisibility("public");
+		// Creator
+		EventAttendee creator = new EventAttendee().setEmail(creatorEmail);
+		creator.setOrganizer(true);
+		event.setAttendees(Arrays.asList(creator));
+		calendarAdmin.events().insert("primary", event).setSendNotifications(true).execute();
+		log.log(Level.FINE, "Event created");
+	}
+
+	public Integer eventAttendeesNumber(String eventId) throws IOException {
+		Event event = calendarAdmin.events().get("primary", eventId).execute();
+		if (event.getAttendees() != null) {
+			return event.getAttendees().size();
+		} else {
+			return 0;
+		}
+	}
+
+	public com.google.api.services.calendar.model.Events getEvents() throws IOException {
+		return calendarAdmin.events().list("primary").execute();
+	}
+
+	public void sendInvitationToUser(String email, String eventId) throws IOException {
+		Event event = calendarAdmin.events().get("primary", eventId).execute();
+		List<EventAttendee> attendees = event.getAttendees();
+		if (attendees == null) {
+			attendees = new ArrayList<EventAttendee>();
+		}
+		attendees.add(new EventAttendee().setEmail(email));
+		Event changes = new Event().setAttendees(attendees);
+		calendarAdmin.events().patch("primary", eventId, changes).setSendNotifications(true).execute();
+		log.log(Level.FINE, "Invitation sent to user: " + email);
 	}
 
 }
