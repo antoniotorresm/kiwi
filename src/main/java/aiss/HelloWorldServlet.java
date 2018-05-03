870e0ee3e9c4232168a9a8e67cab70d6f92a66a3
Antonio Torres Moríñigo <atorresm@protonmail.com>
2018-05-03 22:46:57 +0200

    Add Google OAuth user login

--- src/main/java/aiss/HelloWorldServlet.java
+++ src/main/java/aiss/HelloWorldServlet.java
@@ -1,17 +1,13 @@
 package aiss;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import aiss.model.resource.TwitterResource;
-import twitter4j.Status;
-import twitter4j.TwitterException;
 
 public class HelloWorldServlet extends HttpServlet {
 
@@ -21,7 +17,7 @@ public class HelloWorldServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final Logger log = Logger.getLogger(HelloWorldServlet.class.getName());
 
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 
 		// Sample log
 		log.log(Level.FINE, "Processing GET request");
@@ -30,19 +26,20 @@ public class HelloWorldServlet extends HttpServlet {
 		resp.getWriter().println("Hello world!");
 
 		// Google Calendar API Test
-		//GoogleCalendarResource resource = new GoogleCalendarResource();
-		//resp.getWriter().println(resource.getPrimaryCalendar().getSummary());
+		// GoogleCalendarResource resource = new GoogleCalendarResource();
+		// resp.getWriter().println(resource.getPrimaryCalendar().getSummary());
 		// Twitter test
-		TwitterResource twitter;
-		try {
-			twitter = new TwitterResource();
-			List<Status> tweets = twitter.query("#test");
-			for (Status status : tweets.subList(0, 10)) {
-				resp.getWriter().println(status.getUser().getName() + ": " + status.getText());
-			}
-		} catch (TwitterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// TwitterResource twitter;
+		// try {
+		// twitter = new TwitterResource();
+		// List<Status> tweets = twitter.query("#test");
+		// for (Status status : tweets.subList(0, 10)) {
+		// resp.getWriter().println(status.getUser().getName() + ": " +
+		// status.getText());
+		// }
+		// } catch (TwitterException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
 	}
 }
